5743e4206d99c45746dadd502feee4af89686836
Andrew Crabb <ahc@me.com>
2017-02-18 17:42:39 -0500

    Capistrano deploy.rb now symlinks application.yml which is the Figaro secrets file

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,7 +46,7 @@ set :passenger_restart_with_touch, true
 # Skip migration if files in db/migrate were not modified
 set :conditionally_migrate, true
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/application.yml')
 
 
 namespace :deploy do
