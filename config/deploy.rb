ca73d1d88c1d19b509c24801066f6afd5cfe1894
Andrew Crabb <andrew@crabb.org>
2018-11-29 00:01:26 -0500

    puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,27 @@ set :repo_url, 'ssh://git-codecommit.us-east-1.amazonaws.com/v1/repos/idoimaging
 # https://github.com/capistrano/rails
 # set :passenger_restart_with_touch, true
 # Now the recommended way 11/18.  capistrano-passenger install notes.
-set :passenger_restart_with_touch, false
+# set :passenger_restart_with_touch, false
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to true if using ActiveRecord
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
 
 # Defaults to false
 # Skip migration if files in db/migrate were not modified
@@ -52,7 +72,6 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 
 
 namespace :deploy do
-
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
@@ -61,5 +80,37 @@ namespace :deploy do
       # end
     end
   end
+
+    desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+
 
 end
