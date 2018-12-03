dd1e71846868668f4ddd09bf15aae0f758ea924c
Andrew Crabb <andrew@crabb.org>
2018-12-02 20:20:30 -0500

    models/version verify date

--- app/models/version.rb
+++ app/models/version.rb
@@ -2,6 +2,8 @@ class Version < ActiveRecord::Base
   audited
   belongs_to :program
 
+  validates :date, presence: true
+
   # scope :latest3, -> { order("date desc").limit(3) }
   scope :latest         , ->(n = 1)  { where.not(date: nil).order("date desc").limit(n) }
   scope :latest_programs, ->(n = 10) { includes(:program).where.not(date: nil).order('date desc').first(n) }
--- bin/check_github.rb
+++ bin/check_github.rb
@@ -26,75 +26,81 @@ def make_github_string(url)
   return ret
 end
 
-def analyze_release(id, rel)
-  # pp rel
-  # pp rel.tag_name
-  # pp rel.name
-  # pp rel.published_at
-  puts sprintf("%-30s: %-10s %s\n", id, rel.tag_name, rel.published_at)
-end
-
 def find_latest_tag(client, github_id, tags)
   latest_tag = nil
   if tags.count.nonzero?
-    thash = {}
     # puts "------ tags -------"
-    tags.each do |tag|
-      # pp tag
-      # commit = tag[:commit]
-      commit_sha = tag[:commit][:sha]
-      commit = client.commit(github_id, commit_sha)
+    thash = tags.map do |tag|
+      commit = client.commit(github_id, tag[:commit][:sha])
       commit_date = commit[:commit][:author][:date]
-      # puts "%-10s %s" % [tag[:name], commit_date]
-      thash[commit_date] = tag[:name]
-    end
+      [commit_date, tag[:name].strip]
+    end.to_h
     latest_tag = thash.sort.last
   end
   return latest_tag
 end
 
+# Combine release name with its tag name unless identical
+
+def make_release_name(release)
+  [release[:tag_name], release[:name]].uniq.join(' ').strip
+end
+
 def find_latest_release(client, github_id, releases)
+  latest_release = nil
   if releases.count.nonzero?
-    latest = client.latest_release(github_id)
-    latest_release = [latest.published_at, latest.tag_name]
-    # analyze_release(github_id, latest_release)
+    rhash = releases.map { |rel| [rel[:published_at], make_release_name(rel)] }.to_h
+    latest_release = rhash.sort.last
   end
   return latest_release
 end
 
+def find_latest_release_orig(client, github_id, releases)
+  puts "#{github_id}: #{releases.count} releases"
+  if releases.count.nonzero?
+    begin
+      latest = client.latest_release(github_id)
+      latest_release = [latest.published_at, latest.tag_name]
+    rescue
+      puts "Exception: latest_release(#{github_id})"
+    end
+  end
+  return latest_release
+end
+
+# latest may be nil
+
+def compare_latest_version(prog_id, latest)
+  prog = Program.find(prog_id)
+  versions = prog.versions.where.not(date: nil).order(:date)
+  latest_version_date = versions.first.date    # Date
+  latest_str = ''
+  if latest
+    latest_date = latest[0].to_date
+    is_newer = latest_date <=> latest_version_date
+    latest_str = " #{latest_date} '#{latest_date.class}' #{is_newer}"
+  end
+  puts "#{prog_id} #{latest_version_date} '#{latest_version_date.class}' #{latest_str}"
+end
+
 # Test release date of each program that has a github resource
 
-source_urls = Resource.source_urls.github.limit(30)
+source_urls = Resource.source_urls.github.limit(10)
 puts "#{source_urls.count} source_urls"
 source_urls.each do |resource|
-  # pp resource
   prog_id = resource.resourceful_id
+  # next unless prog_id.eql? 23
   github_id = make_github_string(resource.url)
   releases = client.releases(github_id)
   tags = client.tags(github_id)
   repo = client.repository(github_id)
   latest_release = find_latest_release(client, github_id, releases)
   latest_tag = find_latest_tag(client, github_id, tags)
+  latest = [latest_release, latest_tag].select{ |e| e}.sort{ |a, b| a[0] <=> b[0] }.last
   outstr = "%4d %-30s: %3d releases, %3d tags" % [resource.resourceful_id, github_id, releases.count, tags.count]
   outstr += " latest %-7s %-10s %-6s" % ["release", latest_release[0], latest_release[1]] if latest_release
   outstr += " latest %-7s %-10s %-6s" % ["tag", latest_tag[0], latest_tag[1]] if latest_tag
+  outstr += " winner %-10s '%s'" % [latest[0], latest[1]] if latest
   puts outstr
-
-  # tags.each do |tag|
-  #   pp tag
-  # end
+  compare_latest_version(prog_id, latest)
 end
-
-# revurl = ResourceType.find_by(name: ResourceType::REV_URL)
-# revurl_id = revurl.id
-
-# prog = Program.find_by(name: 'pydicom')
-# puts "Prog: #{prog.id}"
-# prog_revs = prog.resources
-# last_version_rec = prog.versions.order(:date).first
-# last_version = last_version_rec.version
-
-# puts "last_version #{last_version}"
-
-# user = Octokit.user 'pydicom'
-# puts "user: #{user.first}"
